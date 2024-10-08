2ea98689f7c2c9d0b20185e3238b26774cb97dc0
nirajkth <nirajth.k1@gmail.com>
2024-10-08 13:29:46 +0000

    first commit

--- currency-exchange-service/src/main/java/com/neeraj/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/neeraj/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
@@ -13,14 +15,17 @@ public class CircuitBreakerController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    // @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+    @RateLimiter(name = "default")
     public String sampleApi() {
         LOGGER.info("Sample API is called");
 
-        var forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-                String.class);
+        // var forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+        //         String.class);
 
-        return forEntity.getBody();
+        // return forEntity.getBody();
+        return "sample-api";
     }
 
     private String hardcodedResponse(Exception ex) {
